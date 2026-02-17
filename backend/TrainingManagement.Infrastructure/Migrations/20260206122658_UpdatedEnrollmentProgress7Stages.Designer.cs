e42edcbaff1f6c31e13ac80d19ac59e8a66f1d3b
multiplechuks <multiplechuks@yahoo.com>
2026-02-17 13:43:52 +0200

    updated the migrations

--- backend/TrainingManagement.Infrastructure/Migrations/20260206122658_UpdatedEnrollmentProgress7Stages.Designer.cs
+++ backend/TrainingManagement.Infrastructure/Migrations/20260206122658_UpdatedEnrollmentProgress7Stages.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TrainingManagement.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using TrainingManagement.Infrastructure.Data;
 namespace TrainingManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(TrainingDbContext))]
-    partial class TrainingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260206122658_UpdatedEnrollmentProgress7Stages")]
+    partial class UpdatedEnrollmentProgress7Stages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -264,10 +267,6 @@ namespace TrainingManagement.Infrastructure.Migrations
                     b.Property<string>("Description")
                         .HasMaxLength(500)
                         .HasColumnType("nvarchar(500)");
-
-                    b.Property<string>("Frequency")
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
 
                     b.Property<string>("Name")
                         .IsRequired()
